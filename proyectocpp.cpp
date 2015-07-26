2d2cfbe716e06e71c4bfd9c9bfd67ce37cb9b9ce
ChositoVanecitaforever <cristronick@gmail.com>
2015-07-26 14:44:46 -0500

    agregada de archivos y adelante de la primera opcion

--- proyectocpp.cpp
+++ proyectocpp.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 #include<conio>
-#include<string>
-#include<stdio.h>
+#include<fstream>
+#include<stdlib>
+#include<time>
+
 
 main()
 {
-
+ofstream archivo("C:\Users\MONICA\Documents\ProyLPEquipo7\horario.txt");
+	archivo<<"\n";
+	archivo.close();
 int opc;
+char materia[20],semestre[20],dia[20],hinicio[20],hfin[20];
 
   cout<<"\>>MENU PRINCIPAL<<\n";
-  cout<<"Actividades:";
+  cout<<"Actividades:\n";
   cout<<"1 Ingresar una nueva hora de clases\n";
   cout<<"2 Ver el horario de hoy\n";
   cout<<"3 Ver el horario de la semana\n";
@@ -19,12 +24,23 @@ int opc;
   cin>>opc;
   while (opc>4)
   {
+   cout<< "Ingrese una opcion en el intervalo del menu: \n" ;
   	cin>>opc;
   }
    switch(opc)
      {
   	case 1:
-   	cout<<"Ingrese ingrese una nueva hora de clases: \n";
+
+      	cout<<"Ingrese los siguientes parametors:\n";
+      	cout<<"Nombre de la materia a agregar\n";
+      	cin>>materia;
+
+      	cout<<"Nivel del semestre\n";
+      	cin>>semestre;
+
+      	cout<<"Dia de la semana\n";
+         cin>>dia;
+
 
       break ;
    case 2:
